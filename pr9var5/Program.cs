6563049727dc8bc01783042139d731dfd89c7476
ИСП-22 <-22@LAB30-05>
2024-12-09 13:47:20 +0300

    Добавьте файлы проекта.

---
+++ pr9var5/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Дан массив 30 значений в диапазоне [-100;100] найти количество 
+положительных и отрицательных*/
+
+int[] array = new int[30];  //создаем массив
+Random random = new Random();
+for (int i = 0; i < array.Length; i++)  //заолняем массив случайными значениями
+{
+    array[i] = random.Next(-100, 101);
+}
+
+int positiveCount = array.Count( x => x > 0 );  //положительные
+int negativeCount = array.Count(x => x < 0);    //отрицательные
+
+//вывод
+Console.WriteLine($"Кол-во положительных чисел: {positiveCount}");
+Console.WriteLine($"Кол-во отрицательных чисел: {negativeCount}");
